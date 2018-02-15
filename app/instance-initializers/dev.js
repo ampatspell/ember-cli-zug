bdff04b02a542649e059fa61dd20e288f7e2a39a
ampatspell <ampatspell@gmail.com>
2018-02-15 06:14:38 +0200

    cleanup

--- app/instance-initializers/dev.js
+++ app/instance-initializers/dev.js
@@ -1,36 +1,14 @@
 export default {
   name: 'dev',
   initialize(app) {
-    app.inject('component', 'store', 'service:store');
-    app.inject('route', 'store', 'service:store');
+    app.inject('component', 'store',  'service:store');
     app.inject('component', 'router', 'service:router');
+    app.inject('route',     'store',  'service:store');
 
-    let store = app.lookup('service:store');
+    let store    = app.lookup('service:store');
     let firebase = app.lookup('service:firebase');
 
-    window.store = store;
+    window.store    = store;
     window.firebase = firebase;
-
-    // let main = store.fork('main');
-    // let query = main.query({
-    //   id: 'all-posts',
-    //   query: db => db.collection('posts').orderBy('created_at', 'asc')
-    // });
-    // query.load().then(arg => {
-    //   console.log('loaded', arg.get('content'));
-    // });
-
-    // let ref = db.doc('posts/sU1cbdwiLm80CpUI3TlR');
-    // ref.onSnapshot(snapshot => {
-    //   console.log(snapshot);
-    // });
-
-    // let posts = db.collection('posts');
-    // posts.orderBy('created_at', 'desc').limit(1).onSnapshot(snapshot => {
-    //   console.log(snapshot);
-    //   let doc = snapshot.docs[0];
-    //   console.log(doc);
-    // });
-
   }
 };
