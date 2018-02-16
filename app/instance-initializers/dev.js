84dff33abc6350ffab4ccb1c9e7669ed7a3ea33d
ampatspell <ampatspell@gmail.com>
2018-02-16 02:42:59 +0200

    support async firebase init based on /init.json

--- app/instance-initializers/dev.js
+++ app/instance-initializers/dev.js
@@ -1,12 +1,5 @@
 import registerStoreService from 'models/register-store-service';
 
-const opts = {
-  apiKey: "AIzaSyDyjC_rsH7_BYJwjKgIrHhoSvRBfNnjGrQ",
-  authDomain: "ohne-zeit.firebaseapp.com",
-  projectId: "ohne-zeit",
-  storageBucket: ""
-};
-
 export default {
   name: 'dev',
   initialize(app) {
@@ -14,7 +7,7 @@ export default {
     app.inject('component', 'router', 'service:router');
     app.inject('route',     'store',  'service:store');
 
-    let store = registerStoreService(app, opts);
+    let store = registerStoreService(app);
     window.store = store;
   }
 };
