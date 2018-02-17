3f1d897ebb3a189f6f69cdd35f1205a118651e17
Arnis Vuskans <ampatspell@gmail.com>
2018-02-17 19:49:38 +0200

    figuring out document api

--- tests/unit/document-test.js
+++ tests/unit/document-test.js
@@ -6,11 +6,32 @@ module('document', {
   beforeEach() {
     this.coll = this.firestore.collection('ducks');
     this.recreate = () => recreateCollection(this.coll);
-    this.create = () => this.store._internal.documents.createNewDocument();
+    this.create = props => this.store._internal.documents.createNewDocument(props);
   }
 });
 
 test('create a document', function(assert) {
-  let doc = this.create();
-  assert.ok(true);
+  let doc = this.create({ collection: 'ducks', id: 'yellow', data: { name: 'Yellow' } });
+
+  assert.deepEqual(doc.getProperties('id', 'collection', 'path'), {
+    id: 'yellow',
+    collection: 'ducks',
+    path: 'ducks/yellow'
+  });
+
+  assert.deepEqual(doc.get('data.serialized'), {
+    name: 'Yellow'
+  });
+});
+
+test('create document without data', function(assert) {
+  let doc = this.create({ collection: 'ducks', id: 'yellow' });
+
+  assert.deepEqual(doc.getProperties('id', 'collection', 'path'), {
+    id: 'yellow',
+    collection: 'ducks',
+    path: 'ducks/yellow'
+  });
+
+  assert.deepEqual(doc.get('data.serialized'), {});
 });
