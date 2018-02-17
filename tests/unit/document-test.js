6b17d7e5939d9083d6fbc3558b0a927ad6d726f3
Arnis Vuskans <ampatspell@gmail.com>
2018-02-17 20:23:24 +0200

    local reference with mutatable id, collection, path

--- tests/unit/document-test.js
+++ tests/unit/document-test.js
@@ -35,3 +35,39 @@ test('create document without data', function(assert) {
 
   assert.deepEqual(doc.get('data.serialized'), {});
 });
+
+test('create document with path', function(assert) {
+  let doc = this.create({ path: 'ducks/yellow' });
+  assert.deepEqual(doc.getProperties('id', 'collection', 'path'), {
+    id: 'yellow',
+    collection: 'ducks',
+    path: 'ducks/yellow'
+  });
+});
+
+test('create document with collection', function(assert) {
+  let doc = this.create({ collection: 'ducks' });
+  assert.deepEqual(doc.getProperties('id', 'collection', 'path'), {
+    id: undefined,
+    collection: 'ducks',
+    path: undefined
+  });
+});
+
+test('create blank document', function(assert) {
+  let doc = this.create({});
+  assert.deepEqual(doc.getProperties('id', 'collection', 'path'), {
+    id: undefined,
+    collection: undefined,
+    path: undefined
+  });
+});
+
+test('create document with id', function(assert) {
+  let doc = this.create({ id: 'yellow' });
+  assert.deepEqual(doc.getProperties('id', 'collection', 'path'), {
+    id: 'yellow',
+    collection: undefined,
+    path: undefined
+  });
+});
