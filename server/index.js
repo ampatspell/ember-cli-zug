0ae5980df90e7a08490575e1a6fa0bac28e6b72a
Arnis Vuskans <ampatspell@gmail.com>
2018-02-18 23:37:49 +0200

    make travis to use different config

--- server/index.js
+++ server/index.js
@@ -3,15 +3,29 @@
 
 module.exports = app => {
 
-  const dev = {
-    "apiKey": "AIzaSyDyjC_rsH7_BYJwjKgIrHhoSvRBfNnjGrQ",
-    "databaseURL": "https://ohne-zeit.firebaseio.com",
-    "storageBucket": "ohne-zeit.appspot.com",
-    "authDomain": "ohne-zeit.firebaseapp.com",
-    "messagingSenderId": "491555737764",
-    "projectId": "ohne-zeit"
+  let dev = {
+    apiKey: "AIzaSyDyjC_rsH7_BYJwjKgIrHhoSvRBfNnjGrQ",
+    databaseURL: "https://ohne-zeit.firebaseio.com",
+    storageBucket: "ohne-zeit.appspot.com",
+    authDomain: "ohne-zeit.firebaseapp.com",
+    messagingSenderId: "491555737764",
+    projectId: "ohne-zeit"
   };
 
-  app.get('/__/firebase/init.json', (req, res) => res.json(dev));
+  let ci = {
+    apiKey: "AIzaSyAOqRA96vnTlCnvGvb7PtwyARfvxSx7gDA",
+    authDomain: "ohne-zeit-test.firebaseapp.com",
+    databaseURL: "https://ohne-zeit-test.firebaseio.com",
+    projectId: "ohne-zeit-test",
+    storageBucket: "ohne-zeit-test.appspot.com",
+    messagingSenderId: "1043845604698"
+  };
+
+  let config = process.env.CI ? ci : dev;
+
+  console.log('Travis:', !!process.env.CI);
+  console.log(config);
+
+  app.get('/__/firebase/init.json', (req, res) => res.json(config));
 
 };
