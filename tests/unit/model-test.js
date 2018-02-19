b2b8233f546b2d69b2de650f8e67602bc2ccbb4a
Arnis Vuskans <ampatspell@gmail.com>
2018-02-19 10:53:26 +0200

    register models in identity

--- tests/unit/model-test.js
+++ tests/unit/model-test.js
@@ -1,12 +1,14 @@
 import module from '../helpers/module-for-firebase';
 import { test } from '../helpers/qunit';
 import Model from 'models/model';
+import { run } from '@ember/runloop';
 
 const Duck = Model.extend();
 
 module('model', {
   beforeEach() {
     this.register('model:duck', Duck);
+    this.identity = this.store._internal.identity.models;
     this.create = name => this.store._internal.models.createModel(name);
   }
 });
@@ -16,4 +18,14 @@ test('create model', function(assert) {
   assert.ok(model);
   assert.ok(model._internal);
   assert.ok(Duck.detectInstance(model));
+});
+
+test('model is registered in context', function(assert) {
+  let model = this.create('duck');
+  assert.ok(this.identity._storage.all.includes(model._internal));
+
+  run(() => this.store.destroy());
+
+  assert.ok(model.isDestroying);
+  assert.ok(model._internal.isDestroyed);
 });
