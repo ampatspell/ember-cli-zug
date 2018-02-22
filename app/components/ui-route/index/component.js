49190985eaad4f7fdd2b8843ebebe0f68c8b5db1
Arnis Vuskans <ampatspell@gmail.com>
2018-02-22 14:20:17 +0200

    app uses query computed prop

--- app/components/ui-route/index/component.js
+++ app/components/ui-route/index/component.js
@@ -1,5 +1,17 @@
 import Component from '@ember/component';
+import { query } from 'models/model/computed';
 
 export default Component.extend({
+
+  query: query({
+    context: 'store',
+    id: 'all-people',
+    query: db => db.collection('people').orderBy('__name__', 'asc')
+  }),
+
+  didInsertElement() {
+    this._super();
+    this.get('query').load();
+  }
 
 });
--- app/models/people.js
+++ app/models/people.js
@@ -1,15 +1,11 @@
 import TransientModel from 'models/model/transient';
-import { computed } from '@ember/object';
+import { query } from 'models/model/computed';
 
 export default TransientModel.extend({
 
-  // query: query({
-  //   id: 'people-by-name',
-  //   query: db => db.collection('people').orderBy('name')
-  // }),
-
-  query: computed(function() {
-    return this.get('context').query({ id: 'people-by-name', query: db => db.collection('people').orderBy('name', 'asc') });
-  }),
+  query: query({
+    id: 'people-by-name',
+    query: db => db.collection('people').orderBy('name', 'asc')
+  })
 
 });
