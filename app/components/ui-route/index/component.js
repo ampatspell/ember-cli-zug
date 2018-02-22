274e92e09271b65a24d86132d99f93cd2a20bb00
Arnis Vuskans <ampatspell@gmail.com>
2018-02-22 21:53:33 +0200

    app

--- app/components/ui-route/index/component.js
+++ app/components/ui-route/index/component.js
@@ -10,7 +10,7 @@ export default Component.extend({
   }),
 
   collection: 'people',
-  order: '__name__',
+  order: 'name',
 
   query: query(function() {
     let { collection, order } = this.getProperties('collection', 'order');
