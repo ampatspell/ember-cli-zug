c1b39e05001d04fae3ea161d6360f133b4bea4fb
Arnis Vuskans <ampatspell@gmail.com>
2018-02-26 18:55:24 +0200

    move instanceof firestore.* to firestore-types

--- app/components/ui-route/experimental/component.js
+++ app/components/ui-route/experimental/component.js
@@ -1,17 +1,23 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { query } from 'models/model/computed';
 
 export default Component.extend({
   classNameBindings: [ ':ui-route-experimental' ],
 
+  store: service(),
+
   arrayFromArray: query({
     id: 'array-from-array',
     type: 'array',
+    context: 'store',
     query: db => db.collection('people').orderBy('name', 'asc')
   }),
 
   arrayFromSingle: query({
     id: 'array-from-single',
     type: 'array',
+    context: 'store',
     query: db => db.doc('people/ampatspell')
   })
 
