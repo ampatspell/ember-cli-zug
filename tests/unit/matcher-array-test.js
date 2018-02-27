ce46b0f9db9f34bb1035e99d9e783bcbdd79b5bf
Arnis Vuskans <ampatspell@gmail.com>
2018-02-28 01:15:07 +0200

    array matcher

---
+++ tests/unit/matcher-array-test.js
@@ -0,0 +1,58 @@
+import module from '../helpers/module-for-firebase';
+import { test } from '../helpers/qunit';
+import PersistedModel from 'models/model/persisted';
+import { id } from 'models/model/persisted/computed';
+
+const Duck = PersistedModel.extend({
+  id: id()
+});
+
+module('matcher-array', {
+  async beforeEach() {
+    this.modelNameForDocument = () => 'duck';
+    this.register('model:duck', Duck);
+    this.create = opts => this.store._internal.matchersManager.createInternalMatcher(opts);
+  }
+});
+
+test('match and rematch array', function(assert) {
+  let yellow = this.store.model({ name: 'duck', id: 'yellow', collection: 'ducks', data: { friendId: null } });
+  let green = this.store.model({ name: 'duck', id: 'green', collection: 'ducks', data: { friendId: null } });
+
+  let matcher = this.create({
+    type: 'array',
+    model: [ 'doc.data.friendId' ],
+    matches(model) {
+      return model.get('doc.data.friendId') === 'green'
+    }
+  });
+
+  let content = matcher.content;
+
+  assert.deepEqual(content.mapBy('id'), []);
+
+  yellow.set('doc.data.friendId', 'green');
+  assert.deepEqual(content.mapBy('id'), [ 'yellow' ]);
+
+  green.set('doc.data.friendId', 'green');
+  assert.deepEqual(content.mapBy('id'), [ 'yellow', 'green' ]);
+
+  this.store.model({ name: 'duck', id: 'red', collection: 'ducks', data: { friendId: 'green' } });
+
+  assert.deepEqual(content.mapBy('id'), [ 'yellow', 'green', 'red' ]);
+});
+
+test('initial match array', function(assert) {
+  let yellow = this.store.model({ name: 'duck', id: 'yellow', collection: 'ducks', data: { friendId: 'green' } });
+  this.store.model({ name: 'duck', id: 'green', collection: 'ducks', data: {} });
+
+  let matcher = this.create({
+    type: 'array',
+    model: [ 'doc.data.friendId' ],
+    matches(model) {
+      return model.get('doc.data.friendId') === 'green'
+    }
+  });
+
+  assert.deepEqual(matcher.content.mapBy('id'), [ 'yellow' ]);
+});
