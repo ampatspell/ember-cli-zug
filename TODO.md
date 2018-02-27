6bde951be3ef0e68bc9cd04f44abc46a292495bb
Arnis Vuskans <ampatspell@gmail.com>
2018-02-28 01:44:04 +0200

    basic match computed prop impl

--- TODO.md
+++ TODO.md
@@ -9,33 +9,3 @@
 * fastboot shoebox for models and documents
 * firebase session
 * firebase storage
-
-``` javascript
-import PersistedModel from 'models/model/persisted';
-import { id, attr, promise } from 'models/model/persisted/computed';
-import { match } from 'models/model/computed';
-
-export default PersistedModel.extend({
-
-  id: id(),
-
-  name:  attr(),
-  email: attr(),
-
-  blogs: match({
-    type: 'single',
-    context: 'context',
-    owner: [ 'doc.id' ],
-    model: [ 'modelName', 'doc.data.authorId' ],
-    matches(model, owner) {
-      if(model.get('modelName') !== 'blog') {
-        return;
-      }
-      return owner.get('doc.id') === model.get('doc.data.authorId');
-    }
-  }),
-
-  save: promise('save')
-
-});
-```
--- tests/unit/match-test.js
+++ tests/unit/match-test.js
@@ -2,13 +2,25 @@ import module from '../helpers/module-for-firebase';
 import { test } from '../helpers/qunit';
 import PersistedModel from 'models/model/persisted';
 import { id } from 'models/model/persisted/computed';
-import { match } from 'models/model/computed';
+import { match } from 'models/computed';
 
 const Duck = PersistedModel.extend({
   id: id(),
   friend: match({
+    type: 'single',
     owner: [ 'doc.id' ],
-    model: [ 'doc.friendId' ]
+    model: [ 'doc.data.friendId' ],
+    matches(model, owner) {
+      return model.get('doc.data.friendId') === owner.get('id');
+    }
+  }),
+  friends: match({
+    type: 'array',
+    owner: [ 'doc.id' ],
+    model: [ 'doc.data.friendId' ],
+    matches(model, owner) {
+      return model.get('doc.data.friendId') === owner.get('id');
+    }
   })
 });
 
@@ -19,10 +31,36 @@ module('match', {
   }
 });
 
-test.skip('hello', function(assert) {
+test('initial single match', function(assert) {
   let yellow = this.store.model({ name: 'duck', id: 'yellow', collection: 'ducks', data: { friendId: 'green' } });
-  assert.ok(!yellow.get('friend'));
+  let green = this.store.model({ name: 'duck', id: 'green', collection: 'ducks', data: {} });
 
-  let green = this.store.model({ name: 'duck', id: 'yellow', collection: 'ducks', data: {} });
-  assert.ok(yellow.get('friend') === green);
+  assert.ok(green.get('friend') === yellow);
+});
+
+test('single match', function(assert) {
+  let yellow = this.store.model({ name: 'duck', id: 'yellow', collection: 'ducks', data: {} });
+  let green = this.store.model({ name: 'duck', id: 'green', collection: 'ducks', data: {} });
+
+  assert.ok(green.get('friend') === null);
+
+  yellow.set('doc.data.friendId', 'green');
+  assert.ok(green.get('friend') === yellow);
+});
+
+test('array match', function(assert) {
+  let yellow = this.store.model({ name: 'duck', id: 'yellow', collection: 'ducks', data: {} });
+  let green = this.store.model({ name: 'duck', id: 'green', collection: 'ducks', data: {} });
+  let red = this.store.model({ name: 'duck', id: 'red', collection: 'ducks', data: {} });
+
+  assert.deepEqual(red.get('friends').mapBy('id'), []);
+
+  yellow.set('doc.data.friendId', 'red');
+  assert.deepEqual(red.get('friends').mapBy('id'), [ 'yellow' ]);
+
+  green.set('doc.data.friendId', 'red');
+  assert.deepEqual(red.get('friends').mapBy('id'), [ 'yellow', 'green' ]);
+
+  yellow.set('doc.data.friendId', null);
+  assert.deepEqual(red.get('friends').mapBy('id'), [ 'green' ]);
 });
