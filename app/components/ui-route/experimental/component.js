a00f72f87bb487ceaf75647e2a91eba61ed5ac09
Arnis Vuskans <ampatspell@gmail.com>
2018-02-28 02:15:03 +0200

    app

--- app/components/ui-route/experimental/component.js
+++ app/components/ui-route/experimental/component.js
@@ -1,11 +1,5 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-route-experimental' ],
-
-  model: computed(function() {
-    return this.get('store').model({ name: 'thing', data: { info: { name: 'thingie' } } });
-  })
-
+  classNameBindings: [ ':ui-route-experimental' ]
 });
--- app/models/thing.js
+++
@@ -1,8 +0,0 @@
-import PersistedModel from 'models/model/persisted';
-import { attr } from 'models/model/persisted/computed';
-
-export default PersistedModel.extend({
-
-  name: attr({ key: 'info.name' }),
-
-});
