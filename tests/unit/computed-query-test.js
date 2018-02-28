172335e7bbf262c6b81da32c8635d44e0393def8
Arnis Vuskans <ampatspell@gmail.com>
2018-02-28 16:05:34 +0200

    query property reuse test

--- tests/unit/computed-query-test.js
+++ tests/unit/computed-query-test.js
@@ -4,6 +4,8 @@ import TransientModel from 'models/model/transient';
 import PersistentModel from 'models/model/persisted';
 import { query } from 'models/computed';
 import { recreateCollection } from '../helpers/runloop';
+import { cacheFor } from 'models/-private/util/destroyable-computed';
+import { run } from '@ember/runloop';
 
 const State = TransientModel.extend({
 
@@ -15,7 +17,19 @@ const State = TransientModel.extend({
   duck: query({
     type: 'single',
     query: db => db.collection('ducks').orderBy('name')
-  })
+  }),
+
+  order: 'name',
+
+  dependent: query(function() {
+    let order = this.get('order');
+    return {
+      id: `by-${order}`,
+      type: 'array',
+      owner: [ 'order' ],
+      query: db => db.collection('ducks').orderBy(order)
+    }
+  }),
 
 });
 
@@ -30,9 +44,9 @@ module('computed-query', {
 
     this.coll = this.firestore.collection('ducks');
     this.recreate = () => recreateCollection(this.coll, [
-      { __name__: 'yellow', name: 'yellow' },
-      { __name__: 'green',  name: 'green' },
-      { __name__: 'red',    name: 'red' }
+      { __name__: 'yellow', name: 'yellow', pos: 0 },
+      { __name__: 'green',  name: 'green',  pos: 1 },
+      { __name__: 'red',    name: 'red',    pos: 2 }
     ]);
   }
 });
@@ -65,4 +79,30 @@ test('load single', async function(assert) {
 
   assert.equal(duck.get('isLoading'), false);
   assert.equal(duck.get('content.doc.id'), 'green');
+});
+
+test('property is reused, query -- recreated', async function(assert) {
+  let state = this.store.model({ name: 'state' });
+
+  let first = state.get('dependent');
+  let firstProp = cacheFor(state, 'dependent');
+
+  assert.equal(first.get('id'), 'by-name');
+
+  await first.load();
+  assert.deepEqual(first.get('content').mapBy('doc.id'), [ "green", "red", "yellow" ]);
+
+  run(() => state.set('order', 'pos'));
+
+  assert.ok(first.isDestroyed);
+
+  let second = state.get('dependent');
+  let secondProp = cacheFor(state, 'dependent');
+  assert.ok(second !== first);
+  assert.ok(firstProp === secondProp);
+
+  assert.equal(second.get('id'), 'by-pos');
+
+  await second.load();
+  assert.deepEqual(second.get('content').mapBy('doc.id'), [ "yellow", "green", "red" ]);
 });
