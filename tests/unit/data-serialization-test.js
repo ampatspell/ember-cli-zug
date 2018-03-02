33d1959a31acb6251f192e7c6f6f9b36c052f7b5
Arnis Vuskans <ampatspell@gmail.com>
2018-03-02 23:47:53 +0200

    add withPropertyChanges handling for objects

--- tests/unit/data-serialization-test.js
+++ tests/unit/data-serialization-test.js
@@ -24,29 +24,32 @@ test('create with object', function(assert) {
 
 test('object replace with object', function(assert) {
   let doc = this.create({ person: { name: 'duck' } });
+
   assert.deepEqual(doc.toJSON(), {
     "person": {
       "name": "duck"
     }
   });
-  // assert.deepEqual(doc.get('serialized'), {
-  //   "person": {
-  //     "name": "duck"
-  //   }
-  // });
+
+  assert.deepEqual(doc.get('serialized'), {
+    "person": {
+      "name": "duck"
+    }
+  });
 
   doc.set('person', { email: 'duck@gmail.com' });
+
   assert.deepEqual(doc.toJSON(), {
     "person": {
       "email": "duck@gmail.com"
     }
   });
 
-  // assert.deepEqual(doc.get('serialized'), {
-  //   "person": {
-  //     "email": "duck@gmail.com"
-  //   }
-  // });
+  assert.deepEqual(doc.get('serialized'), {
+    "person": {
+      "email": "duck@gmail.com"
+    }
+  });
 });
 
 test('object set primitive value', function(assert) {
@@ -55,9 +58,9 @@ test('object set primitive value', function(assert) {
 
   assert.deepEqual(doc.toJSON(), {
   });
-  // assert.deepEqual(doc.get('serialized'), {
-  //   name: 'zeeba'
-  // });
+
+  assert.deepEqual(doc.get('serialized'), {
+  });
 
   doc.set('name', 'zeeba');
   assert.equal(doc.get('name'), 'zeeba');
@@ -65,25 +68,30 @@ test('object set primitive value', function(assert) {
   assert.deepEqual(doc.toJSON(), {
     name: 'zeeba'
   });
-  // assert.deepEqual(doc.get('serialized'), {
-  //   name: 'zeeba'
-  // });
+
+  assert.deepEqual(doc.get('serialized'), {
+    name: 'zeeba'
+  });
 });
 
 test('object replace primitive value with primitive value', function(assert) {
   let doc = this.create();
   assert.equal(doc.get('name'), undefined);
 
+  assert.deepEqual(doc.get('serialized'), {
+  });
+
   doc.set('name', 'zeeba');
   assert.equal(doc.get('name'), 'zeeba');
+
+  assert.deepEqual(doc.get('serialized'), {
+    name: 'zeeba'
+  });
 
   doc.set('name', 'larry');
   assert.equal(doc.get('name'), 'larry');
 
-  assert.deepEqual(doc.toJSON(), {
+  assert.deepEqual(doc.get('serialized'), {
     name: 'larry'
   });
-  // assert.deepEqual(doc.get('serialized'), {
-  //   name: 'larry'
-  // });
 });
