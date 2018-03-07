f8fd5a641acfb829ac3644932c6229c4175e3d2c
Arnis Vuskans <ampatspell@gmail.com>
2018-03-07 02:12:23 +0200

    app

--- app/components/ui-route/crud/detail/show/component.js
+++ app/components/ui-route/crud/detail/show/component.js
@@ -1,7 +1,7 @@
 import Component from '../base/component';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-route-crud-detail-selection' ],
+  classNameBindings: [ ':ui-route-crud-detail-show' ],
 
   componentNamePrefix: 'show',
 
