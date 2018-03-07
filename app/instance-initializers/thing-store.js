3a324472a939a62391b9f4717306c753a1aeabfe
Arnis Vuskans <ampatspell@gmail.com>
2018-03-07 22:00:24 +0200

    broken, experimental

--- app/instance-initializers/thing-store.js
+++ app/instance-initializers/thing-store.js
@@ -1,37 +1,51 @@
 import registerContextService from 'models/register-context-service';
 
+const firebase = {
+  apiKey: "AIzaSyDyjC_rsH7_BYJwjKgIrHhoSvRBfNnjGrQ",
+  databaseURL: "https://ohne-zeit.firebaseio.com",
+  storageBucket: "ohne-zeit.appspot.com",
+  authDomain: "ohne-zeit.firebaseapp.com",
+  messagingSenderId: "491555737764",
+  projectId: "ohne-zeit"
+};
+
+const modelNameForDocument = (document, context) => {
+  let { collection } = document.getProperties('collection');
+
+  if(context.get('absoluteIdentifier').startsWith('store/crud')) {
+    if(collection === 'people') {
+      return 'crud/person';
+    }
+    if(collection === 'blogs') {
+      return 'crud/blog';
+    }
+  }
+
+  if(collection === 'people') {
+    return 'person';
+  }
+  if(collection === 'blogs') {
+    return 'blog';
+  }
+
+  return 'unknown';
+};
+
+const storeNameForIdentifier = absoluteIdentifier => {
+  if(absoluteIdentifier === 'store') {
+    return 'store';
+  }
+};
+
 export default {
   name: 'thing:store',
   initialize(app) {
     app.inject('component', 'router', 'service:router');
 
     registerContextService(app, {
-      modelNameForDocument(document, context) {
-        let { collection } = document.getProperties('collection');
-
-        if(context.get('absoluteIdentifier').startsWith('store/crud')) {
-          if(collection === 'people') {
-            return 'crud/person';
-          }
-          if(collection === 'blogs') {
-            return 'crud/blog';
-          }
-        }
-
-        if(collection === 'people') {
-          return 'person';
-        }
-        if(collection === 'blogs') {
-          return 'blog';
-        }
-
-        return 'unknown';
-      },
-      storeNameForIdentifier(absoluteIdentifier) {
-        if(absoluteIdentifier === 'store') {
-          return 'store';
-        }
-      }
+      firebase,
+      modelNameForDocument,
+      storeNameForIdentifier
     });
   }
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     addons: {
-      blacklist: [ 'ember-cli-fastboot' ]
+      // blacklist: [ 'ember-cli-fastboot' ]
     }
   });
 
