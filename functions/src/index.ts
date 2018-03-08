8b12bbc915f82cdb07a8cbb83398e6776b6db25a
Arnis Vuskans <ampatspell@gmail.com>
2018-03-08 11:33:35 +0200

    experiments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+// import * as fastbootMiddleware from 'fastboot-express-middleware';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// let dist = './dist';
+
+let app = express();
+// app.use(express.static(dist, { index: false }));
+// app.get('/*', fastbootMiddleware(dist));
+
+// exports.fastboot = functions.https.onRequest(app);
+
+app.get('/', (req, res) => {
+  res.json({ path: '/' });
+});
+
+app.get('*', (req, res) => {
+  res.json({ path: '*' });
+});
+
+exports.hello = functions.https.onRequest((req, res) => {
+  if(!req.path) {
+    req.url = `/${req.url}`;
+  }
+  return app(req, res);
+});
