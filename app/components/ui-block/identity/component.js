a425854e85d376f86312022ea7d6b3f73ae0cc0d
Arnis Vuskans <ampatspell@gmail.com>
2018-03-08 10:42:36 +0200

    disable identity in fastboot

--- app/components/ui-block/identity/component.js
+++ app/components/ui-block/identity/component.js
@@ -1,8 +1,18 @@
 import Component from '@ember/component';
+import { getOwner } from '@ember/application';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   classNameBindings: [ ':ui-block-identity' ],
 
   context: null,
+
+  isEnabled: computed(function() {
+    let fastboot = getOwner(this).lookup('service:fastboot');
+    if(!fastboot) {
+      return true;
+    }
+    return !fastboot.get('isFastBoot');
+  }),
 
 });
