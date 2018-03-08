509cec644eeeeea8b91f232c8e45efe0bd3efaa9
Arnis Vuskans <ampatspell@gmail.com>
2018-03-08 20:03:09 +0200

    move fastboot to per-context

--- app/instance-initializers/thing-dev.js
+++ app/instance-initializers/thing-dev.js
@@ -1,11 +1,6 @@
 export default {
   name: 'thing:dev',
   after: 'thing:store',
-  initialize(app) {
-    let stores = app.lookup('models:stores');
-    let fastboot = stores._internal.fastbootManager;
-    if(!fastboot.isFastBoot) {
-      window.shoebox = fastboot.payload;
-    }
+  initialize() {
   }
 };
