cd6c63dfd32c2e5cdfd7b014e539d9e324a5d7b6
ampatspell <ampatspell@gmail.com>
2018-04-01 01:16:01 +0300

    call super

--- addon/-private/auth/internal-user.js
+++ addon/-private/auth/internal-user.js
@@ -3,6 +3,7 @@ import Internal from '../model/internal';
 export default class InternalUser extends Internal {
 
   constructor(context, auth) {
+    super();
     this.context = context;
     this.auth = auth;
   }
