6a870b708ad8a9d3d76e02388dbf5e11b9f36453
ampatspell <ampatspell@gmail.com>
2018-04-01 01:59:32 +0300

    dummy

--- tests/dummy/app/components/ui-route/experimental/component.js
+++ tests/dummy/app/components/ui-route/experimental/component.js
@@ -1,5 +1,17 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNameBindings: [ ':ui-route-experimental' ]
+  classNameBindings: [ ':ui-route-experimental' ],
+
+  actions: {
+    signIn() {
+      console.log('sign in');
+      this.get('store.auth.methods.anonymous').signIn();
+    },
+    signOut() {
+      console.log('sign out');
+      this.get('store.auth').signOut();
+    }
+  }
+
 });
