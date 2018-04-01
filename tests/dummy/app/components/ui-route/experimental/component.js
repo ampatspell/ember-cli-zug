d52ec29b1baac6eb9d4f2a8d5b62e3ca51696ed1
ampatspell <ampatspell@gmail.com>
2018-04-01 04:43:13 +0300

    dummy

--- tests/dummy/app/components/ui-route/experimental/component.js
+++ tests/dummy/app/components/ui-route/experimental/component.js
@@ -4,12 +4,30 @@ export default Component.extend({
   classNameBindings: [ ':ui-route-experimental' ],
 
   actions: {
-    signIn() {
+    signInAnonymous() {
+      this.willSignIn();
       this.get('store.auth.methods.anonymous').signIn();
+    },
+    signInEmail(email, password) {
+      this.willSignIn();
+      this.get('store.auth.methods.email').signIn(email, password).catch(err => {
+        this.signInDidFail(err);
+      });
     },
     signOut() {
       this.get('store.auth').signOut();
     }
+  },
+
+  willSignIn() {
+    this.set('error', null);
+  },
+
+  signInDidFail(err) {
+    if(this.isDestroying) {
+      return;
+    }
+    this.set('error', err);
   }
 
 });
