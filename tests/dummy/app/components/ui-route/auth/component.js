65822327db9aa1d79f862415659ed5e6807a6a25
ampatspell <ampatspell@gmail.com>
2018-04-01 20:25:09 +0300

    move auth ui to ui-route/auth. leave experimental empty

---
+++ tests/dummy/app/components/ui-route/auth/component.js
@@ -0,0 +1,39 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNameBindings: [ ':ui-route-auth' ],
+
+  actions: {
+    signInAnonymous() {
+      this.willSignIn();
+      this.get('store.auth.methods.anonymous').signIn();
+    },
+    signInEmail(email, password) {
+      this.willSignIn();
+      this.get('store.auth.methods.email').signIn(email, password).catch(err => {
+        this.signInDidFail(err);
+      });
+    },
+    signUpEmail(email, password) {
+      this.willSignIn();
+      this.get('store.auth.methods.email').signUp(email, password).catch(err => {
+        this.signInDidFail(err);
+      });
+    },
+    signOut() {
+      this.get('store.auth').signOut();
+    }
+  },
+
+  willSignIn() {
+    this.set('error', null);
+  },
+
+  signInDidFail(err) {
+    if(this.isDestroying) {
+      return;
+    }
+    this.set('error', err);
+  }
+
+});
