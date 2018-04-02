b502d2b4a048fb756c87eba8354378dc486e3ae2
ampatspell <ampatspell@gmail.com>
2018-04-02 18:42:03 +0300

    add basic random string function in utils

--- addon/utils.js
+++ addon/utils.js
@@ -1,9 +1,11 @@
 import firebase from 'firebase';
 import { isFieldValue, isGeoPoint } from './-private/util/firestore-types';
+import makeID from './-private/util/random-string';
 
 export const serverTimestamp = () => firebase.firestore.FieldValue.serverTimestamp();
 
 export {
   isFieldValue,
-  isGeoPoint
+  isGeoPoint,
+  makeID
 };
